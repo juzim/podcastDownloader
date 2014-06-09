cf61c1d0ae68a96472f25d5fa7595c50e470f076
yeahbert <yeahbert@gmail.com>
2014-06-09 21:50:36 +0200

    Fixed pushbullet integration; use requests instead of urllib

--- podcastDownloader.py
+++ podcastDownloader.py
@@ -1,11 +1,25 @@
 import re
 import feedparser
-import urllib2
+import requests
 import os
 import datetime
 import logging
 import pushbullet
 import configparser
+
+## todo:
+## add limit
+## add multiple sources with renaming
+## get list of devices when not given
+
+def pushToPB( message, phone ):
+    if usePushBullet != "1" or phone is None:
+        return
+    push = phone.push_note( 'Podcast downloader', message )
+    if push.status_code != 200:
+        logging.error('[ERR] Could not push notification: ' + push.status_code)
+    else:
+        logging.info('Pushed notification to device')
 
 logging.basicConfig(filename='.talDownloader.log',
                     level=logging.INFO,
@@ -15,7 +29,6 @@ logging.basicConfig(filename='.talDownloader.log',
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 logging.getLogger('').addHandler(console)
-
 
 config = configparser.ConfigParser()
 config.sections()
@@ -39,22 +52,22 @@ config.read('settings.conf')
 baseDir = config['global']['podcasts_dir']
 usePushBullet = config['global']['use_pushbullet']
 pushBulletApi = config['pushBullet']['api_key']
-deviceId = config['pushBullet']['device_key']
+deviceId = int(config['pushBullet']['device_key'])
 
 if baseDir == "":
     print("Please add a directory for the podcasts to the config file")
     exit()
 
-if usePushBullet == 1:
+if usePushBullet == "1":
     if pushBulletApi == "" or deviceId == "":
-        print("Pushbullet is enabled, but no settings are given")
+        logging.error("Pushbullet is enabled, but no settings are given")
         exit()
     pb = pushbullet.PushBullet(pushBulletApi)
     phone = pb.get(deviceId)
-    usePushBullet = 1
+else:
+    phone = None
 
 d = os.path.dirname(baseDir + "This_American_Life/")
-
 logging.info('[BEG] Starting to parse feed')
 
 if not os.path.exists(baseDir):
@@ -62,7 +75,7 @@ if not os.path.exists(baseDir):
     os.makedirs(d)
 
 if not os.path.exists(d):
-    logging.info('[INF] Folder for xxx not found, creating')
+    logging.info('[INF] Folder not found, creating')
     os.makedirs(d)
 
 feed = feedparser.parse("http://feeds.thisamericanlife.org/talpodcast")
@@ -73,20 +86,18 @@ for i in items:
     title = datetime.datetime.strptime(i.published, '%a, %d %b %Y %X +0000').strftime('%Y_%m_%d') + '_' + re.sub('[#!:]', '', name)
     file = d + '/' + title + '.mp3'
     if os.path.isfile(file):
-        logging.info("[SKP] File " + name + " already exists")
+        logging.info("[SKP] File " + file + " already exists")
         continue
-    try:
-        logging.info("[INF] New file found: " + title)
-        remoteFile = urllib2.urlopen(i.media_content[0]['url'])
-        with open(file, "wb") as local_file:
-            local_file.write(remoteFile.read())
-        logging.info("File successfully downloaded")
-        if usePushBullet == 1:
-            push = phone.push_note('Podcast downloader', 'New episode downloaded: ' + i.title)
-            if push.status_code != 200:
-                logging.error('[ERR] Could not push notification: ' + push.status_code)
-        break
-    except urllib2.error.UrlError as e:
-        logging.error('[ERR] Could not download file: ' + e)
-
+    logging.info("[INF] New file found: " + title)
+    remoteFile = requests.get(i.media_content[0]['url'])
+    if not remoteFile.ok:
+        logging.error('[ERR] Could not download file: ' + str(remoteFile.error))
+        pushToPB('File could not be downloaded: ' + i.title, phone)
+        continue
+    with open(file, "wb") as local_file:
+        local_file.write(remoteFile.content)
+    logging.info("[SUC] File successfully downloaded")
+    pushToPB('New episode downloaded: ' + i.title, phone)
 logging.info('[END] Done processing feed')
+exit()
+
